2bbaefe0e9a9e6d7551f2c0bb2435dacc2fff699
0chuhh <vvlasovdo@mail.ru>
2022-10-05 11:17:20 +0900

    'initial'

---
+++ app/src/main/java/com/example/glava3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.glava3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+        Toast myToast = Toast.makeText(getApplicationContext(),"Уведомление", Toast.LENGTH_LONG);
+        myToast.show();
+    }
+}
